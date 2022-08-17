b9b01923b82a1b25ce7e6c60c83e35a9e38d1620
Shingai Shamu <mshamu95@gmail.com>
2022-08-17 02:03:17 +0200

    added logic

---
+++ looker_blueprints/create_sql_query.py
@@ -0,0 +1,65 @@
+import argparse
+import sys
+import shipyard_utils as shipyard
+try:
+    import helpers
+except BaseException:
+    from . import helpers
+from looker_sdk import methods40, models40
+
+
+EXIT_CODE_LOOK_QUERY_ERROR = 203
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--base-url', dest='base_url', required=True)
+    parser.add_argument('--client-id', dest='client_id', required=True)
+    parser.add_argument('--client-secret', dest='client_secret', required=True)
+    parser.add_argument('--connection-name', dest='connection_name', required=True)
+    parser.add_argument('--model-name', dest='model_name', required=True)
+    parser.add_argument('--sql', dest='sql', required=True)
+    args = parser.parse_args()
+    return args
+
+
+def create_sql_query(look_sdk, connection_name, model_name, sql_query):
+    sql_body = models40.SqlQueryCreate(
+        connection_name=connection_name,
+        model_name=model_name,
+        sql=sql_query
+    )
+    try:
+        res_slug = look_sdk.create_sql_query(
+            body=sql_body
+        ).slug
+        print("Looker slug {res_slug} created successfully")
+    except Exception as e:
+        print(f'Error running create query: {e}')
+        sys.exit(EXIT_CODE_LOOK_QUERY_ERROR)
+    return res_slug
+
+
+def main():
+    args = get_args()
+    base_url = args.base_url
+    client_id = args.client_id
+    client_secret = args.client_secret
+    connection_name = args.connection_name
+    model_name = args.model_name
+    sql = args.sql
+
+    # generate SDK
+    look_sdk = helpers.get_sdk(base_url, client_id, client_secret)
+    
+    # create sql query
+    new_slug = create_sql_query(look_sdk, connection_name, model_name, sql)
+    
+    # save slug to pickle file
+    artifact_subfolder_paths = helpers.artifact_subfolder_paths
+    shipyard.logs.create_pickle_file(artifact_subfolder_paths, 
+                                                   'slug', new_slug)
+
+
+if __name__ == "__main__":
+    main()
+    
---
+++ looker_blueprints/helpers.py
@@ -0,0 +1,45 @@
+import os
+import shipyard_utils as shipyard
+import looker_sdk
+from looker_sdk import api_settings
+
+
+# create Artifacts folder paths
+base_folder_name = shipyard.logs.determine_base_artifact_folder(
+    'looker')
+artifact_subfolder_paths = shipyard.logs.determine_artifact_subfolders(
+    base_folder_name)
+shipyard.logs.create_artifacts_folders(artifact_subfolder_paths)
+
+
+class MyApiSettings(api_settings.ApiSettings):
+    def __init__(self, *args, **kw_args):
+        self.base_url = kw_args.pop('base_url')
+        self.client_id = kw_args.pop("client_id")
+        self.client_secret = kw_args.pop("client_secret")
+        super().__init__(*args, **kw_args)
+
+    def read_config(self):
+        config = super().read_config()
+        config['base_url'] = self.base_url
+        config["client_id"] = self.client_id
+        config["client_secret"] = self.client_secret
+        # return config settings class
+        return config
+
+
+def get_sdk(base_url, client_id, client_secret):
+    """Creates a looker v4.0 SDK for use in query methods
+    
+    Returns:
+        sdk: Looker 4.0 SDK Class
+    """
+    # initialize looker sdk
+    sdk = looker_sdk.init40(
+    config_settings=MyApiSettings(
+        base_url=base_url,
+        client_id=client_id,
+        client_secret=client_secret
+        )
+    )
+    return sdk
