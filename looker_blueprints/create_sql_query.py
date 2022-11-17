e2a872ebee35cb7106b2f2fc4808101a61b012b1
wrp801 <wespoulsen@gmail.com>
2022-11-16 17:12:37 -0700

    Reconfigured CLI args

--- looker_blueprints/create_sql_query.py
+++ looker_blueprints/create_sql_query.py
@@ -17,8 +17,8 @@ def get_args():
     parser.add_argument('--base-url', dest='base_url', required=True)
     parser.add_argument('--client-id', dest='client_id', required=True)
     parser.add_argument('--client-secret', dest='client_secret', required=True)
-    parser.add_argument('--connection-name', dest='connection_name', required=False)
-    parser.add_argument('--model-name', dest='model_name', required=False)
+    parser.add_argument("--connection-target", dest = 'connection_target',required = True,choices={"connection_name","model_name"})
+    parser.add_argument("--connection-value", dest = 'connection_value', required=True)
     parser.add_argument('--sql', dest='sql', required=True)
     args = parser.parse_args()
     return args
@@ -36,8 +36,7 @@ def create_sql_query(look_sdk, connection_name, model_name, sql_query):
         ).slug
         print(f"Looker slug {res_slug} created successfully")
     except Exception as e:
-        # print(e.args)
-        print(f'Error running create query: {e.__str__()}')
+        print(f'Error running create query: {e}')
         sys.exit(ec.EXIT_CODE_LOOK_QUERY_ERROR)
     return res_slug
 
@@ -47,21 +46,32 @@ def main():
     base_url = args.base_url
     client_id = args.client_id
     client_secret = args.client_secret
-    connection_name = args.connection_name
-    model_name = args.model_name
+    connection_target = args.connection_target
+    connection_value = args.connection_value
     sql = args.sql
 
     # generate SDK
     look_sdk = helpers.get_sdk(base_url, client_id, client_secret)
     
     # create sql query
-    new_slug = create_sql_query(look_sdk, connection_name, model_name, sql)
-    
-    # save slug to pickle file
-    artifact_subfolder_paths = helpers.artifact_subfolder_paths
-    shipyard.logs.create_pickle_file(artifact_subfolder_paths, 
-                                                   'slug', new_slug)
+    if connection_target == 'connection_name':
+        sql_body = models40.SqlQueryCreate(connection_name= connection_value,model_name=None ,sql = sql)
 
+    elif connection_target == 'model_name':
+        sql_body = models40.SqlQueryCreate(model_name= connection_value, connection_name= None ,sql= sql)
+    
+    try:
+        new_slug = look_sdk.create_sql_query(body = sql_body).slug
+        print(f"Looker slug {new_slug} created successfully")
+        # save slug to pickle file
+        artifact_subfolder_paths = helpers.artifact_subfolder_paths
+        shipyard.logs.create_pickle_file(artifact_subfolder_paths, 
+                                                    'slug', new_slug)
+
+    except Exception as e:
+        print(f"Error running create query: {e}")
+        sys.exit(ec.EXIT_CODE_LOOK_QUERY_ERROR)
+    
 
 if __name__ == "__main__":
     main()
